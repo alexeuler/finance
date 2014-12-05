147d573da40b0083094ed0e9b1731838e3afc05d
alex <alex@nrg>
2014-12-05 14:05:53 +0300

    added blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141205110400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blog_posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.text     "tags"
+    t.string   "status"
+    t.string   "image"
+    t.string   "category"
+    t.text     "excerpt"
+    t.string   "slug"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
