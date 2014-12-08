44608c28db10c449f65f34df3bdf1ec050077d7d
alex <alex@nrg>
2014-12-08 18:20:33 +0300

    added languange to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206213815) do
+ActiveRecord::Schema.define(version: 20141208151908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141206213815) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "language"
   end
 
   create_table "blog_tags", force: true do |t|
