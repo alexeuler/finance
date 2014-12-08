681d8ada6955d587760683e4bea0e73d7bffa899
alex <alex@nrg>
2014-12-08 19:06:26 +0300

    added tag update

--- app/models/blog/tag.rb
+++ app/models/blog/tag.rb
@@ -1,10 +1,13 @@
 class Blog::Tag < ActiveRecord::Base
   def self.update_all
     # extracting tags array from posts
-    tag_models = Blog::Post.select(:tags).to_a
-    tag_strings = tag_models.map {|tag| tag[:tags]}
-    tag_string = tag_strings.join(" ")
-    tags = tag_string.split(" ")
+    separator = "|_|"
+    tag_models = Blog::Post.select(:tags, :language).to_a
+    tag_groups = tag_models.map do |tag_model|
+      post_tags = tag_model[:tags].split(" ")
+      post_tags.map {|post_tag| post_tag+separator+tag_model[:language]}
+    end
+    tags = tag_groups.flatten
     tags.uniq!
     tags.sort!
 
@@ -12,6 +15,9 @@ class Blog::Tag < ActiveRecord::Base
     Blog::Tag.delete_all
 
     #Fill tags models
-    tags.each {|tag| Blog::Tag.create(value: tag)}
+    tags.each do |tag|
+      tag_and_lang = tag.split(separator)
+      Blog::Tag.create(value: tag_and_lang[0], language: tag_and_lang[1])
+    end
   end
 end
