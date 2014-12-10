001e2604739e079bf39144c52c871199d9be97e5
alex <alex@nrg>
2014-12-10 17:27:16 +0300

    added db_clone to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,8 @@
 # property set.  Don't declare `role :all`, it's a meta role.
 
 set :application, 'finance_staging'
+set :root, "/var/www/#{fetch(:application)}/current"
+
 
 website_url='lovimagaz.ru'
 ssh_user='deployment'
@@ -12,7 +14,7 @@ ssh_user='deployment'
 
 role :app, [ssh_user+"@"+website_url]
 role :web, [ssh_user+"@"+website_url]
-role :db,  [ssh_user+"@"+website_url]
+role :db, [ssh_user+"@"+website_url]
 
 
 # Extended Server Syntax
@@ -49,3 +51,24 @@ server website_url, user: ssh_user, roles: %w{web app}, my_property: :my_value
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+
+
+namespace :db do
+  task :clone do
+    on ssh_user+"@"+website_url do
+      db_dump = "#{fetch(:root)}/tmp/db_dump"
+      execute "sudo /etc/init.d/thin stop_single /etc/thin/#{fetch(:application)}.yml"
+      execute "pg_dump finance_production > #{db_dump}"
+      execute 'dropdb finance_staging'
+      execute 'createdb finance_staging'
+      execute "psql finance_staging < #{db_dump}"
+      execute "rm #{db_dump}"
+      execute "sudo /etc/init.d/thin start_single /etc/thin/#{fetch(:application)}.yml"
+    end
+  end
+end
+
+namespace :deploy do
+  after :published, 'db:clone'
+end
+
