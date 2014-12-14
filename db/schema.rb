d4c83f72a658c31a013fe021c9d229f80c49b6ee
alex@laptop <karasev.alexey@laptop>
2014-12-14 09:10:52 +0300

    added image tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211132640) do
+ActiveRecord::Schema.define(version: 20141214060933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,12 @@ ActiveRecord::Schema.define(version: 20141211132640) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "language",   default: "ru", null: false
+  end
+
+  create_table "image_tags", force: true do |t|
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "images", force: true do |t|
