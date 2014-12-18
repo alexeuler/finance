6ea24b05dee60abdd26e63b753ef63d30a00f219
alex <alex@nrg>
2014-12-18 13:40:10 +0300

    added default values

--- app/models/blog/post.rb
+++ app/models/blog/post.rb
@@ -1,5 +1,6 @@
 class Blog::Post < ActiveRecord::Base
   after_commit :update_tags
+  after_initialize :default_values
   belongs_to :video_group, :class_name => 'Video::Group'
 
   scope :video, -> { where(breed: 'video') }
@@ -10,4 +11,9 @@ class Blog::Post < ActiveRecord::Base
   def update_tags
     Blog::Tag.update_all
   end
+
+  def default_values
+    self.breed ||= 'text'
+  end
+
 end
