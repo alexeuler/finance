b099a9999792cd82a9bcb4bd1f2530b7f81e123a
alex@laptop <karasev.alexey@laptop>
2015-01-02 21:10:38 +0300

    added email config

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,16 @@ module Finance
 
 
     end
+
+    config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: '-i -t'
+# }
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.default_options = {from: 'no-reply@ka-ching.ru'}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
