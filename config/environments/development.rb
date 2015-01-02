d84a59f8b841c933acf9f6331a87be26ef0ed5f6
alex@laptop <karasev.alexey@laptop>
2015-01-02 21:30:21 +0300

    added email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.default_options =
-      {from: 'no-reply@localhost',
-       host: 'localhost'}
+  config.action_mailer.default_options = {from: 'no-reply@localhost'}
+  config.action_mailer.default_url_options = {host: 'localhost'}
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_options =
-      {from: 'no-reply@ka-ching.ru',
-       host: 'ka-ching.ru'}
+  config.action_mailer.default_options = {from: 'no-reply@ka-ching.ru'}
+  config.action_mailer.default_url_options = {host: 'ka-ching.ru'}
 
 end
