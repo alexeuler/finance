c4ed9d7914fa76d9dde6945d4b8cef25502e5993
alex@laptop <karasev.alexey@laptop>
2015-04-01 06:38:03 +0300

    fixed link issue

--- app/helpers/blog/posts_helper.rb
+++ app/helpers/blog/posts_helper.rb
@@ -1,2 +1,9 @@
 module Blog::PostsHelper
+  def blog_post_link(post)
+    if post.slug and not post.slug.empty?
+      "#{blog_root_path}/#{post.slug}"
+    else
+      blog_post_path(post)
+    end
+  end
 end
